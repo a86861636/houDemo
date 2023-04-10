fb9e02bdd92ce658dcb21f64f86942c451826b6b
曾伟智 <zengwz@szlanyou.com>
2023-04-10 11:45:04 +0800

    副标题bug

--- src/components/datav1/index.vue
+++ src/components/datav1/index.vue
@@ -1,98 +1,98 @@
 <template>
-  <div id="data-view1">
-    <dv-full-screen-container>
-      <div class="main-header">
-        <div class="mh-left" v-if="type == 1">
-          <dv-border-box-8 class="button">
-            指标总量
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            同比增速
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            环比增速
-          </dv-border-box-8>
-        </div>
-        <div class="mh-left" v-if="type == 2">
-          <dv-border-box-8 class="button">
-            临汾开发区排序
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            全省开发区排序
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            全省开发区增速排序
-          </dv-border-box-8>
-        </div>
-        <div class="mh-left" v-if="type == 3">
-          <dv-border-box-8 class="button">
-            三产结构
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            三产结构增速
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            产业优势
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            政策优势
-          </dv-border-box-8>
-        </div>
-        <div class="mh-left" v-if="type == 4">
-          <dv-border-box-8 class="button">
-            企业数量及增速
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            产业规模
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            产业结构占比
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            战略新兴产业
-          </dv-border-box-8>
-        </div>
-        <div class="mh-left" v-if="type == 5">
-          <dv-border-box-8 class="button">
-            目标完成情况
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            招商目标完成
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            龙头企业经营情况
-          </dv-border-box-8>
-        </div>
-        <a class="mh-middle" v-if="type == 1">GDP指标</a>
-        <a class="mh-middle" v-if="type == 2">GDP对比指标</a>
-        <a class="mh-middle" v-if="type == 3">产业结构</a>
-        <a class="mh-middle" v-if="type == 4">规上企业</a>
-        <a class="mh-middle" v-if="type == 5">N年经济目标</a>
-        <div class="mh-right">
-        </div>
-      </div>
+	<div id="data-view1">
+		<fullScreen>
+			<div class="main-header">
+				<div class="mh-left" v-if="type == 1">
+					<dv-border-box-8 class="button">
+						指标总量
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						同比增速
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						环比增速
+					</dv-border-box-8>
+				</div>
+				<div class="mh-left" v-if="type == 2">
+					<dv-border-box-8 class="button">
+						临汾开发区排序
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						全省开发区排序
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						全省开发区增速排序
+					</dv-border-box-8>
+				</div>
+				<div class="mh-left" v-if="type == 3">
+					<dv-border-box-8 class="button">
+						三产结构
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						三产结构增速
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						产业优势
+					</dv-border-box-8>
+				</div>
+				<div class="mh-left" v-if="type == 4">
+					<dv-border-box-8 class="button">
+						企业数量及增速
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						产业规模
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						产业结构占比
+					</dv-border-box-8>
+				</div>
+				<div class="mh-left" v-if="type == 5">
+					<dv-border-box-8 class="button">
+						目标完成情况
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						招商目标完成
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						龙头企业经营情况
+					</dv-border-box-8>
+				</div>
+				<a class="mh-middle" v-if="type == 1">GDP指标</a>
+				<a class="mh-middle" v-if="type == 2">GDP对比指标</a>
+				<a class="mh-middle" v-if="type == 3">产业结构</a>
+				<a class="mh-middle" v-if="type == 4">规上企业</a>
+				<a class="mh-middle" v-if="type == 5">N年经济目标</a>
+				<div class="mh-right">
+					<dv-border-box-8 class="button" v-if="type == 3">
+						政策优势
+					</dv-border-box-8>
+					<dv-border-box-8 class="button" v-if="type == 4">
+						战略新兴产业
+					</dv-border-box-8>
+				</div>
+			</div>
 
-      <div class="main-content">
-        <digital-flop />
+			<div class="main-content">
+				<digital-flop />
 
-        <div class="block-left-right-content">
-          <ranking-board />
+				<div class="block-left-right-content">
+					<ranking-board />
 
-          <div class="block-top-bottom-content">
-            <div class="block-top-content">
-              <rose-chart />
+					<div class="block-top-bottom-content">
+						<div class="block-top-content">
+							<rose-chart />
 
-              <water-level-chart />
+							<water-level-chart />
 
-              <scroll-board />
-            </div>
+							<scroll-board />
+						</div>
 
-            <cards />
-          </div>
-        </div>
-      </div>
-    </dv-full-screen-container>
-  </div>
+						<cards />
+					</div>
+				</div>
+			</div>
+		</fullScreen>
+	</div>
 </template>
 
 <script>
@@ -103,100 +103,107 @@ import roseChart from './roseChart'
 import waterLevelChart from './waterLevelChart'
 import scrollBoard from './scrollBoard'
 import cards from './cards'
+import fullScreen from '../full/index.vue'
 
 export default {
-  name: 'DataView',
-  components: {
-    topHeader,
-    digitalFlop,
-    rankingBoard,
-    roseChart,
-    waterLevelChart,
-    scrollBoard,
-    cards
-  },
-  data() {
-    return {
-      type: '1'
-    }
-  },
-   watch:{
-     '$route.query'(val){
-      this.type = val.type
-    }
-  }
+	name: 'DataView',
+	components: {
+		topHeader,
+		digitalFlop,
+		rankingBoard,
+		roseChart,
+		waterLevelChart,
+		scrollBoard,
+		cards,
+		fullScreen
+	},
+	data() {
+		return {
+			type: '1'
+		}
+	},
+	watch: {
+		'$route.query': {
+			immediate: true,
+			handler(val) {
+				this.type = val.type ? val.type : 1
+			}
+		}
+	}
 }
 </script>
 
 <style lang="less" scoped>
 #data-view1 {
-  height: 100%;
-  background-color: #030409;
-  color: #fff;
+	height: 100%;
+	background-color: #030409;
+	color: #fff;
 
-  #dv-full-screen-container {
-    background-image: url('./img/bg.png');
-    background-size: 100% 100%;
-    box-shadow: 0 0 3px blue;
-    display: flex;
-    flex-direction: column;
-  }
+	#dv-full-screen-container {
+		background-image: url('./img/bg.png');
+		background-size: 100% 100%;
+		box-shadow: 0 0 3px blue;
+		display: flex;
+		flex-direction: column;
+	}
 
-  .main-content {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-  }
+	.main-content {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+	}
 
-  .block-left-right-content {
-    flex: 1;
-    display: flex;
-    margin-top: 20px;
-  }
+	.block-left-right-content {
+		flex: 1;
+		display: flex;
+		margin-top: 20px;
+	}
 
-  .block-top-bottom-content {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    box-sizing: border-box;
-    padding-left: 20px;
-  }
+	.block-top-bottom-content {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		box-sizing: border-box;
+		padding-left: 20px;
+	}
 
-  .block-top-content {
-    height: 55%;
-    display: flex;
-    flex-grow: 0;
-    box-sizing: border-box;
-    padding-bottom: 20px;
-  }
+	.block-top-content {
+		height: 55%;
+		display: flex;
+		flex-grow: 0;
+		box-sizing: border-box;
+		padding-bottom: 20px;
+	}
 
-  .main-header {
-    height: 80px;
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-end;
+	.main-header {
+		height: 80px;
+		display: flex;
+		justify-content: space-between;
+		align-items: flex-end;
 
-    .mh-middle {
-      font-size: 30px;
-      line-height: 80px;
-    }
+		.mh-middle {
+			font-size: 30px;
+			line-height: 80px;
+		}
 
-    .mh-left,
-    .mh-right {
-      width: 450px;
-      padding-bottom: 15px;
-      display: flex;
-    }
+		.mh-left,
+		.mh-right {
+			width: 600px;
+			padding-bottom: 15px;
+			display: flex;
+		}
 
-    .button {
-      width: 120px;
-      height: 50px;
-      line-height: 50px;
-      text-align: center;
-      margin-left: 10px;
-      cursor: pointer;
-      white-space: nowrap;
-    }
-  }
+		.button {
+			width: 140px;
+			padding: 0 20px;
+			height: 50px;
+			line-height: 50px;
+			text-align: center;
+			margin-left: 10px;
+			font-size: 15px;
+			cursor: pointer;
+			white-space: nowrap;
+		}
+	}
 }
 </style>
--- src/components/datav2/index.vue
+++ src/components/datav2/index.vue
@@ -1,125 +1,125 @@
 <template>
-  <div id="data-view2">
-    <dv-full-screen-container>
-      <div class="main-header">
-        <div class="mh-left" v-if="type == 1">
-          <dv-border-box-8 class="button">
-            项目总投资额
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            项目数量
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            项目类别
-          </dv-border-box-8>
-        </div>
-        <div class="mh-left" v-if="type == 2">
-          <dv-border-box-8 class="button">
-            本年度建设目标
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            年度目标进度
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            分项目年度进度
-          </dv-border-box-8>
-        </div>
-        <div class="mh-left" v-if="type == 3">
-          <dv-border-box-8 class="button">
-            一产项目规模
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            二产项目规模
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            三产项目规模
-          </dv-border-box-8>
-        </div>
-        <div class="mh-left" v-if="type == 4">
-          <dv-border-box-8 class="button">
-            汽车零部件
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            金属新材料
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            石油钻具
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            生物医药
-          </dv-border-box-8>
-        </div>
-        <div class="mh-left" v-if="type == 5">
-          <dv-border-box-8 class="button">
-            项目概要
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            地理位置
-          </dv-border-box-8>
-          <dv-border-box-8 class="button">
-            项目性质
-          </dv-border-box-8>
-        </div>
-        <a class="mh-middle" v-if="type == 1">项目概况</a>
-        <a class="mh-middle" v-if="type == 2">年度情况</a>
-        <a class="mh-middle" v-if="type == 3">分产业项目</a>
-        <a class="mh-middle" v-if="type == 4">产业链项目</a>
-        <a class="mh-middle" v-if="type == 5">项目建设进度</a>
-        <div class="mh-right">
-          <dv-border-box-8 class="button" v-if="type == 5">
-            总投资
-          </dv-border-box-8>
-          <dv-border-box-8 class="button" v-if="type == 5">
-            项目建设
-          </dv-border-box-8>
-          <dv-border-box-8 class="button" v-if="type == 5">
-            建设预警
-          </dv-border-box-8>
-        </div>
-      </div>
+	<div id="data-view2">
+		<fullScreen>
+			<div class="main-header">
+				<div class="mh-left" v-if="type == 1">
+					<dv-border-box-8 class="button">
+						项目总投资额
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						项目数量
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						项目类别
+					</dv-border-box-8>
+				</div>
+				<div class="mh-left" v-if="type == 2">
+					<dv-border-box-8 class="button">
+						本年度建设目标
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						年度目标进度
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						分项目年度进度
+					</dv-border-box-8>
+				</div>
+				<div class="mh-left" v-if="type == 3">
+					<dv-border-box-8 class="button">
+						一产项目规模
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						二产项目规模
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						三产项目规模
+					</dv-border-box-8>
+				</div>
+				<div class="mh-left" v-if="type == 4">
+					<dv-border-box-8 class="button">
+						汽车零部件
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						金属新材料
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						石油钻具
+					</dv-border-box-8>
+				</div>
+				<div class="mh-left" v-if="type == 5">
+					<dv-border-box-8 class="button">
+						项目概要
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						地理位置
+					</dv-border-box-8>
+					<dv-border-box-8 class="button">
+						项目性质
+					</dv-border-box-8>
+				</div>
+				<a class="mh-middle" v-if="type == 1">项目概况</a>
+				<a class="mh-middle" v-if="type == 2">年度情况</a>
+				<a class="mh-middle" v-if="type == 3">分产业项目</a>
+				<a class="mh-middle" v-if="type == 4">产业链项目</a>
+				<a class="mh-middle" v-if="type == 5">项目建设进度</a>
+				<div class="mh-right">
+					<dv-border-box-8 class="button" v-if="type == 4">
+						生物医药
+					</dv-border-box-8>
+					<dv-border-box-8 class="button" v-if="type == 5">
+						总投资
+					</dv-border-box-8>
+					<dv-border-box-8 class="button" v-if="type == 5">
+						项目建设
+					</dv-border-box-8>
+					<dv-border-box-8 class="button" v-if="type == 5">
+						建设预警
+					</dv-border-box-8>
+				</div>
+			</div>
 
-      <dv-border-box-1 class="main-container">
-        <dv-border-box-3 class="left-chart-container">
+			<dv-border-box-1 class="main-container">
+				<dv-border-box-3 class="left-chart-container">
 
-          <Left-Chart-1 />
-          <Left-Chart-2 />
-          <Left-Chart-3 />
+					<Left-Chart-1 />
+					<Left-Chart-2 />
+					<Left-Chart-3 />
 
-        </dv-border-box-3>
+				</dv-border-box-3>
 
-        <div class="right-main-container">
-          <div class="rmc-top-container">
-            <dv-border-box-3 class="rmctc-left-container">
+				<div class="right-main-container">
+					<div class="rmc-top-container">
+						<dv-border-box-3 class="rmctc-left-container">
 
-              <Center-Cmp />
+							<Center-Cmp />
 
-            </dv-border-box-3>
+						</dv-border-box-3>
 
-            <div class="rmctc-right-container">
-              <dv-border-box-3 class="rmctc-chart-1">
+						<div class="rmctc-right-container">
+							<dv-border-box-3 class="rmctc-chart-1">
 
-                <Right-Chart-1 />
+								<Right-Chart-1 />
 
-              </dv-border-box-3>
+							</dv-border-box-3>
 
-              <dv-border-box-4 class="rmctc-chart-2" :reverse="true">
+							<dv-border-box-4 class="rmctc-chart-2" :reverse="true">
 
-                <Right-Chart-2 />
+								<Right-Chart-2 />
 
-              </dv-border-box-4>
-            </div>
-          </div>
+							</dv-border-box-4>
+						</div>
+					</div>
 
-          <dv-border-box-4 class="rmc-bottom-container">
+					<dv-border-box-4 class="rmc-bottom-container">
 
-            <Bottom-Charts />
+						<Bottom-Charts />
 
-          </dv-border-box-4>
-        </div>
-      </dv-border-box-1>
+					</dv-border-box-4>
+				</div>
+			</dv-border-box-1>
 
-    </dv-full-screen-container>
-  </div>
+		</fullScreen>
+	</div>
 </template>
 
 <script>
@@ -133,129 +133,137 @@ import RightChart1 from './RightChart1'
 import RightChart2 from './RightChart2'
 
 import BottomCharts from './BottomCharts'
+import fullScreen from '../full/index.vue'
 
 export default {
-  name: 'DataView',
-  components: {
-    LeftChart1,
-    LeftChart2,
-    LeftChart3,
-    CenterCmp,
-    RightChart1,
-    RightChart2,
-    BottomCharts
-  },
-  data() {
-    return {
-      type: '1'
-    }
-  },
-  watch: {
-    '$route.query'(val) {
-      this.type = val.type
-    }
-  }
+	name: 'DataView',
+	components: {
+		LeftChart1,
+		LeftChart2,
+		LeftChart3,
+		CenterCmp,
+		RightChart1,
+		RightChart2,
+		BottomCharts,
+		fullScreen
+	},
+	data() {
+		return {
+			type: '1'
+		}
+	},
+	watch: {
+		'$route.query': {
+			immediate: true,
+			handler(val) {
+				this.type = val.type ? val.type : 1
+			}
+		}
+	}
 }
 </script>
 
 <style lang="less">
 #data-view2 {
-  height: 100%;
-  background-color: #030409;
-  color: #fff;
+	height: 100%;
+	background-color: #030409;
+	color: #fff;
 
-  #dv-full-screen-container {
-    background-image: url('./img/bg.png');
-    background-size: 100% 100%;
-    box-shadow: 0 0 3px blue;
-    display: flex;
-    flex-direction: column;
-  }
+	#dv-full-screen-container {
+		background-image: url('./img/bg.png');
+		background-size: 100% 100%;
+		box-shadow: 0 0 3px blue;
+		display: flex;
+		flex-direction: column;
+	}
 
-  .main-header {
-    height: 80px;
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-end;
+	.main-header {
+		height: 80px;
+		display: flex;
+		justify-content: space-between;
+		align-items: flex-end;
 
-    .mh-left {
-      font-size: 20px;
-      color: rgb(1, 134, 187);
+		.mh-left {
+			font-size: 20px;
+			color: rgb(1, 134, 187);
 
-      a:visited {
-        color: rgb(1, 134, 187);
-      }
-    }
+			a:visited {
+				color: rgb(1, 134, 187);
+			}
+		}
 
-    .mh-middle {
-      font-size: 30px;
-      line-height: 80px;
-    }
+		.mh-middle {
+			font-size: 30px;
+			line-height: 80px;
+		}
 
-    .mh-left,
-    .mh-right {
-      width: 450px;
-      padding-bottom: 15px;
-      display: flex;
-      color: white;
-    }
+		.mh-left,
+		.mh-right {
+			width: 600px;
+			padding-bottom: 15px;
+			display: flex;
+			color: white;
+		}
 
-    .button {
-      height: 50px;
-      line-height: 50px;
-      text-align: center;
-      margin-left: 10px;
-      cursor: pointer;
-      white-space: nowrap;
-    }
-  }
+		.button {
+			width: 120px;
+			padding: 0 20px;
+			height: 50px;
+			line-height: 50px;
+			text-align: center;
+			margin-left: 10px;
+			font-size: 15px;
+			cursor: pointer;
+			white-space: nowrap;
+		}
+	}
 
-  .main-container {
-    height: calc(~"100% - 80px");
+	.main-container {
+		height: calc(~"100% - 80px");
 
-    .border-box-content {
-      padding: 20px;
-      box-sizing: border-box;
-      display: flex;
-    }
-  }
+		.border-box-content {
+			padding: 20px;
+			box-sizing: border-box;
+			display: flex;
+		}
+	}
 
-  .left-chart-container {
-    width: 22%;
-    padding: 10px;
-    box-sizing: border-box;
+	.left-chart-container {
+		width: 22%;
+		padding: 10px;
+		box-sizing: border-box;
 
-    .border-box-content {
-      flex-direction: column;
-    }
-  }
+		.border-box-content {
+			flex-direction: column;
+		}
+	}
 
-  .right-main-container {
-    width: 78%;
-    padding-left: 5px;
-    box-sizing: border-box;
-  }
+	.right-main-container {
+		width: 78%;
+		padding-left: 5px;
+		box-sizing: border-box;
+	}
 
-  .rmc-top-container {
-    height: 65%;
-    display: flex;
-  }
+	.rmc-top-container {
+		height: 65%;
+		display: flex;
+	}
 
-  .rmctc-left-container {
-    width: 65%;
-  }
+	.rmctc-left-container {
+		width: 65%;
+	}
 
-  .rmctc-right-container {
-    width: 35%;
-  }
+	.rmctc-right-container {
+		width: 35%;
+	}
 
-  .rmc-bottom-container {
-    height: 35%;
-  }
+	.rmc-bottom-container {
+		height: 35%;
+	}
 
-  .rmctc-chart-1,
-  .rmctc-chart-2 {
-    height: 50%;
-  }
+	.rmctc-chart-1,
+	.rmctc-chart-2 {
+		height: 50%;
+	}
 }
 </style>
